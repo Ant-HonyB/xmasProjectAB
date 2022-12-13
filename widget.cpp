681d5bedb4fe0ce9579ea99459c393470c522312
Anthony Boutard <tony44wind@gmail.com>
2022-12-13 14:51:09 +0100

    base

--- widget.cpp
+++ widget.cpp
@@ -6,6 +6,9 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    mServer = new QTcpServer(this);
+    connect(mServer,SIGNAL(newConnection()),this,SLOT(clientConnected()));
+    mServer->listen(QHostAddress::Any,9090);
 }
 
 Widget::~Widget()
@@ -13,3 +16,24 @@ Widget::~Widget()
     delete ui;
 }
 
+void Widget::clientConnected()
+{
+     QTcpSocket* sockClient = mServer->nextPendingConnection();
+     mClients << sockClient;
+     connect(sockClient,SIGNAL(readyRead()),this,SLOT(dataIsComing()));
+     connect(sockClient,SIGNAL(disconnected()),this,SLOT(clientDisconnected()));
+}
+void Widget::clientDisconnected()
+{
+    QTcpSocket* sock = (QTcpSocket*)sender();
+    mClients.removeAll(sock);
+    sock->deleteLater();
+}
+void Widget::dataIsComing()
+{
+    QTcpSocket* sock = (QTcpSocket*)sender();
+    QByteArray data = sock->readAll();
+
+    for(QTcpSocket* s : mClients)
+        s->write(data);
+}
